e8fbe74bddd5ee20ab3e8b84140f054565c21171
JRaulSierra <j.raulsierra@gmail.com>
2022-09-04 00:05:00 -0600

    install cors and set it in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors);
 
 app.get("/", (req, res) => {
-	res.send("The server is online!!");
+	res.send("The server scout 44 Guatemala is online!!");
 });
 
 app.listen(port, () => {
