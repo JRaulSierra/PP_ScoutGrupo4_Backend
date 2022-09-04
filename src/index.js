a21a1d36450384ee8ad0f00e0623cd5333563200
JRaulSierra <j.raulsierra@gmail.com>
2022-09-03 22:57:53 -0600

    setting eslint and set env port

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('The server is online!')
-})
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("The server is online!!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
